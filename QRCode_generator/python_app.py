2053f2d144073bba62f480230bac06d39ba0ccce
Adrian Duta <adrianchange@gmail.com>
2023-08-26 21:51:53 +0200

    Update python_app.py

--- QRCode_generator/python_app.py
+++ QRCode_generator/python_app.py
@@ -19,6 +19,7 @@ def generate_qrcode(text:str):
     qr.add_data(text)
     qr.make(fit=True)
     img = qr.make_image(fill_color="black", back_color="white")
-    img.save("qrimg.png")
-
-generate_qrcode("https://www.codewithadi.com")
+    img.save("qrimg001.png")
+    
+url = input("Enter your url: "
+generate_qrcode(url)
